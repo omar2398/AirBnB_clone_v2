1cb4bd927d6c4a44d64a51e0c0190026b36e6ff1
Omar mohamed <118669490+omar2398@users.noreply.github.com>
2024-03-11 00:34:02 +0200

    Update 2-do_deploy_web_static.py

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
 # Fabfile to distribute an archive to a web server.
 import os.path
-from fabric.api import env
-from fabric.api import put
-from fabric.api import run
+from fabric.api import env, put, run
+import socket
 
-env.hosts = ["104.196.168.90", "35.196.46.172"]
+def get_own_ip():
+    """Fetches the IP address of the local machine."""
+    return socket.gethostbyname(socket.gethostname())
 
+env.hosts = [get_own_ip()]
 
 def do_deploy(archive_path):
     """Distributes an archive to a web server.
@@ -17,6 +19,9 @@ def do_deploy(archive_path):
         If the file doesn't exist at archive_path or an error occurs - False.
         Otherwise - True.
     """
+    own_ip = get_own_ip()
+    print("Your own IP address is:", own_ip)
+
     if os.path.isfile(archive_path) is False:
         return False
     file = archive_path.split("/")[-1]
